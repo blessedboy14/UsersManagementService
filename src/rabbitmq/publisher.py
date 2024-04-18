8411ce3fa809b0f9e133ba47199313a7679348a5
blessedboy14 <dansof25@mail.ru>
2024-04-18 15:51:10 +0300

    publisher code

---
+++ src/rabbitmq/publisher.py
@@ -0,0 +1,37 @@
+import json
+import pika
+
+
+conf = {'host': 'localhost', 'port': '5672', 'q_name': "reset-password-stream",
+        'login': 'blessedboy', 'password': 'ewkere123'}
+
+
+class Publisher:
+
+    def __init__(self, config: dict):
+        self._config = config
+        self._queue_name = self._config['q_name']
+        self._connection = self.create_connection()
+        self._channel = self._connection.channel()
+        self._publish_q = self._channel.queue_declare(queue=self._queue_name)
+
+    def publish_message(self, message: dict):
+        self._channel.basic_publish(
+            exchange='',
+            routing_key=self._queue_name,
+            properties=pika.BasicProperties(
+            ),
+            body=json.dumps(message)
+        )
+
+    def create_connection(self):
+        params = pika.ConnectionParameters(host=self._config['host'],
+                                           port=self._config['port'],
+                                           credentials=pika.PlainCredentials(
+                                               username=self._config['login'],
+                                               password=self._config['password']
+                                           ))
+        return pika.BlockingConnection(params)
+
+
+publisher = Publisher(conf)
